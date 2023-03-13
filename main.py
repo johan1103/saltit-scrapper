12ca09c9a481559fb416d82593a1d84509b6e9aa
KTH <johan1103@naver.com>
2023-03-13 22:17:18 +0900

    [feat] #2 하나의 행정동에 대해서 스크래핑 기능 구현

--- main.py
+++ main.py
@@ -1,45 +1,78 @@
+import time
+
 import requests
 import base64
 import json
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    print("hello world!")
-    data = {'query': '선릉역', 'page': 0, 'size': 20}
+    data = {'query': '선릉', 'from': 0, 'size': 20}
     URL = 'https://im.diningcode.com/API/isearch/'
-    headers = {
+    chrome_header = {
         'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Connection': 'keep-alive',
-        'Content-Length': '39',
-        'Content-Type': 'gzip, deflate, br',
         'Host': 'im.diningcode.com',
         'Origin': 'https://www.diningcode.com',
         'Referer': 'https://www.diningcode.com/',
-        'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': 'macOS',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-    }
-    inhyeok_header = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'PostmanRuntime / 7.31.1',
+        'User-Agent': 'Mozilla/5.0'
     }
     headersInPostman ={
         'User-Agent': 'PostmanRuntime / 7.31.1',
         'Accept': '* / *',
         'Host': 'im.diningcode.com',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': '39'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     res = requests.post(URL, headers=headersInPostman, data=data)
-    print(res.text)
-    print(res)
-    #print(webpage)
+    resDict = json.loads(res.text)
+    totalCnt = resDict['result_data']['poi_section']['total_cnt']
+    restaurants = {}
+    duplicatedRestaurants = {}
+    for ptr in range(0, int(totalCnt/20)):
+        print(ptr)
+        offset = ptr*20
+        time.sleep(0.5)
+        data = {'query': '선릉', 'from': offset, 'size': offset+20}
+        response = requests.post(URL, headers=chrome_header, data=data)
+        responseDict = json.loads(response.text)
+        for restaurantSummary in responseDict['result_data']['poi_section']['list']:
+            rid = restaurantSummary['v_rid']
+            name = restaurantSummary['nm']
+            imgSrc = restaurantSummary['image']
+            score = restaurantSummary['score']
+            latitude = restaurantSummary['lat']
+            longitude = restaurantSummary['lng']
+            category = restaurantSummary['category']
+            phone = restaurantSummary['phone']
+            address = restaurantSummary['addr']
+            isDuplicated = restaurants.get(restaurantSummary['v_rid'], 'NO_KEY')
+            if isDuplicated == 'NO_KEY':
+                restaurants[rid] = {'rid': rid, 'name': name, 'imgSrc': imgSrc, 'score': score,
+                                    'latitude': latitude, 'longitude': longitude, 'category': category,
+                                    'phone': phone, 'address': address}
+            else:
+                duplicatedRestaurants[rid] = {'rid': rid, 'name': name, 'address': address,
+                                              'duplicatedCnt': 0}
+
+    f = open("static/scrap.txt", 'w')
+    for rid in restaurants:
+        print(rid)
+        f.write('id : ')
+        f.write(restaurants[rid]['rid'])
+        f.write(', name : ')
+        f.write(restaurants[rid]['name'])
+        f.write(', location : {')
+        f.write(str(restaurants[rid]['latitude']))
+        f.write(', ')
+        f.write(str(restaurants[rid]['longitude']))
+        f.write('}')
+        f.write('\n')
+    f.close()
+    print('-----------duplicated----------')
+    f = open("static/duplicated.txt", 'w')
+    for rid in duplicatedRestaurants:
+        print(rid)
+        f.write('id ')
+        f.write(duplicatedRestaurants[rid]['rid'])
+        f.write(' name ')
+        f.write(duplicatedRestaurants[rid]['name'])
+        f.write('\n')
+    f.close()

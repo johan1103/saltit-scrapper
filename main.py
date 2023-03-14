09cb57702bc451a7f7c6766c3cb6dc01d00f5e63
KTH <johan1103@naver.com>
2023-03-15 00:28:56 +0900

    [modify] #6 food_type정보 파일에 저장하도록 수정

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from saveAsFile import save_as_csv
 
 if __name__ == '__main__':
     result_cities = scrapping_city()
+    food_types = ['한식', '중식', '양식', '일식']
     for city_name, city_detail in result_cities.items():
         restaurants = {}
         duplicated_restaurants = {}
         if city_name != "서울":
             break
         for detail_name in city_detail:
-            scrapping_restaurants(restaurants, duplicated_restaurants, detail_name)
-        save_as_text(restaurants, duplicated_restaurants)
+            for food_type in food_types:
+                scrapping_restaurants(restaurants, duplicated_restaurants, detail_name, food_type)
+        save_as_text(restaurants, duplicated_restaurants, city_name)
         save_as_csv(restaurants, duplicated_restaurants, city_name)
--- saveAsFile.py
+++ saveAsFile.py
@@ -5,23 +5,26 @@ def save_as_csv(restaurants, duplicated_restaurants, city_name):
     dict_for_pd = []
     for restaurant_summary in restaurants.values():
         dict_for_pd.append({'address': restaurant_summary['address'], 'rid': restaurant_summary['rid'],
-                            'name': restaurant_summary['name'], 'category': restaurant_summary['category'],
+                            'name': restaurant_summary['name'], 'food_type': restaurant_summary['food_type'],
+                            'category': restaurant_summary['category'],
                             'score': restaurant_summary['score'], 'lat': restaurant_summary['latitude'],
                             'lng': restaurant_summary['longitude'], 'phone': restaurant_summary['phone'],
                             'img': restaurant_summary['imgSrc']})
     df = pd.DataFrame(data=dict_for_pd,
-                      columns=['address', 'rid', 'name', 'category', 'score', 'lat', 'lng', 'phone', 'img'])
+                      columns=['address', 'rid', 'name', 'food_type', 'category', 'score', 'lat', 'lng', 'phone', 'img'])
     df.to_csv(path_or_buf="static/restaurant-summary-" + city_name + ".csv")
     return
 
 
-def save_as_text(restaurants, duplicated_restaurants):
-    f = open("static/scrap.txt", 'w')
+def save_as_text(restaurants, duplicated_restaurants, city_name):
+    f = open("static/scrap-" + city_name + ".txt", 'w')
     for rid in restaurants:
         f.write('id : ')
         f.write(restaurants[rid]['rid'])
         f.write(', name : ')
         f.write(restaurants[rid]['name'])
+        f.write(', foodType : ')
+        f.write(restaurants[rid]['food_type'])
         f.write(', location : {')
         f.write(str(restaurants[rid]['latitude']))
         f.write(', ')

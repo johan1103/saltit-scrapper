ed649fce512ccadf00ec67563d501597a3749977
KTH <johan1103@naver.com>
2023-03-21 13:28:43 +0900

    [modify] #3 insert문 오류 수정 & 배열 오류 수정

--- database/insert_initial.py
+++ database/insert_initial.py
@@ -11,10 +11,10 @@ conn = pymysql.connect(
 
 
 if __name__ == '__main__':
-    food_types = ['한식', '양식', '일식' '중식']
+    food_types = ['한식', '양식', '일식', '중식']
     curs = conn.cursor()
     for food in food_types:
-        sql = f"insert into food_type values (null,{food})"
+        sql = f"insert into food_type(name) values ('{food}')"
         curs.execute(sql)
     select_sql = "select * from food_type"
     curs.execute(select_sql)

106ba9326d6e27685c96b81db1651ce96e0e1eec
KTH <johan1103@naver.com>
2023-03-21 13:31:20 +0900

    [feat] #3 restaurant pk 정보 db에서 select해서 매핑하는 기능구현

---
+++ database/insert_menu.py
@@ -0,0 +1,46 @@
+import pymysql.cursors
+import pandas as pd
+
+# connection 정보
+conn = pymysql.connect(
+    host='localhost',  # host name
+    user='root',  # user name
+    password='1234',  # password
+    db='saltit_test',  # db name
+    charset='utf8'
+)
+
+
+def get_locations_by_tuple(city_name, locations):
+    data = pd.read_csv('../static/summary/restaurant-summary-' + city_name + '.csv')
+    raw_dict = data.to_dict('index')
+    for idx, raw_value in raw_dict.items():
+        locations.append({'rid': raw_value['rid'], 'latitude': raw_value['lat'], 'longitude': raw_value['lng'],
+                          'road_address': raw_value['address']})
+    return
+
+
+def get_restaurant_id_by_dict(curs):
+    dicts = {}
+    sql = "select * from restaurant"
+    curs.execute(sql)
+    result = curs.fetchall()
+    for data in result:
+        dicts[data[3]] = {'id': data[0]}
+    return dicts
+
+
+if __name__ == '__main__':
+    cities = ['강원', '경기', '경남', '경북', '광주', '대구', '대전', '부산', '서울', '세종', '울산', '인천', '전남', '전북',
+              '제주', '충남', '충북']
+    curs = conn.cursor()
+    restaurant_id_dict = get_restaurant_id_by_dict(curs)
+    for city_name in cities:
+        if city_name != '서울':
+            continue
+        location_list = []
+        get_locations_by_tuple(location_list)
+        for location in location_list:
+            rid = location['rid']
+            restaurant_id = restaurant_id_dict[rid]['id']
+            location['restaurant_id'] = restaurant_id
